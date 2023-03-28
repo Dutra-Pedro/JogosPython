a32f602e9d3fb1b07670e4f10fb075e219d4b4ae
Pedro <dutra.pedrosousa@gmail.com>
2023-03-28 18:31:02 -0300

    Manda

--- forca.py
+++ forca.py
@@ -4,6 +4,21 @@ import random
 def jogar():
     print("Bem-vindo ao jogo forca")
 
+palavra_secreta = "banana"
+enforcou = False
+acertou = False
+
+while (not enforcou and not acertou):
+    index = 0
+    chute = input("Digite uma letra:\n")
+    chute = chute.lower().strip()
+    for letra in palavra_secreta:
+        if (chute == letra):
+            print(letra)
+        else:
+            print("_ \t")
+        index += 1
+
 
 if __name__=="__main__":
     jogar()
